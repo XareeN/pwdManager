12a3a7fcb6943dfd664fbf73bea0cb1a9bc7ef47
xareen <xareen@interia.eu>
2019-01-29 01:54:20 +0100

    Added PwdDetailsFragment with layout file Added route to fragment_add_pwd

--- app/src/main/java/com/example/xareen/pwdmanager_2/MyPwdsFragment.java
+++ app/src/main/java/com/example/xareen/pwdmanager_2/MyPwdsFragment.java
@@ -3,6 +3,7 @@ package com.example.xareen.pwdmanager_2;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,18 @@ public class MyPwdsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_pwds, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_pwds, container, false);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_add);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getFragmentManager() != null) {
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddPwdFragment()).commit();
+                }
+            }
+        });
+
+        return view;
     }
 }
